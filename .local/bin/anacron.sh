a771207bd7b27987fa7624f3c0313d7b5e81004f
Mark Stillwell <mark@stillwell.me>
2017-07-17 14:22:45 +0100

    autocommit

--- .local/bin/anacron.sh
+++ .local/bin/anacron.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
-scriptname="$(basename $0)"
+scriptname="$(basename "${BASH_SOURCE[0]}")"
+rundir="${XDG_RUNTIME_DIR:-${HOME}/.local/var/run}/${scriptname}"
+mkdir -p "${rundir}"
+pidfile="${rundir}/PID"
 
-lockdir="${XDG_RUNTIME_DIR:-${HOME}/.local/var/lock}/${scriptname}.lock"
-pidfile="${lockdir}/PID"
-if mkdir ${lockdir} &> /dev/null; then
-    trap "rm -rf ${lockdir}" EXIT SIGHUP SIGINT SIGQUIT SIGTERM
-    echo "$$" > ${pidfile}
-else
-    otherpid=$(cat ${pidfile} 2>/dev/null)
-    othercmd=$(ps --no-headers --format command --pid ${otherpid} 2>/dev/null)
-    if [[ "${othercmd}" =~ .*${scriptname}.* ]]; then
-        if [ $(ps --no-headers -C ${scriptname} | wc -l) -ge 10 ]; then
-            echo "too many ${scriptname} processes waiting for lock"
-            exit 1
-        fi
-        sleep 1
-    else
-        rm -rf ${lockdir}
-    fi
-    exec "$0" "$@"
+exec 200>"${pidfile}"
+if ! flock -n 200 || ! echo "$$" > "${pidfile}"; then
+    echo "another instance of the ${scriptname} is currently running..." >&2
+    exit 1
 fi
+trap 'flock -u 200 && rm -f "${pidfile}"' EXIT SIGHUP SIGINT SIGQUIT SIGTERM
 
 # Set secure permissions on created directories and files
 umask 077
 
-mkdir -p ${XDG_CONFIG_HOME}/anacron/cron.daily
-mkdir -p ${XDG_CONFIG_HOME}/anacron/cron.weekly
-mkdir -p ${XDG_CONFIG_HOME}/anacron/cron.monthly
+mkdir -p "${XDG_CONFIG_HOME}/anacron/cron.daily"
+mkdir -p "${XDG_CONFIG_HOME}/anacron/cron.weekly"
+mkdir -p "${XDG_CONFIG_HOME}/anacron/cron.monthly"
 
-spooldir=${HOME}/.local/var/spool/anacron
-mkdir -p $spooldir
+spooldir="${HOME}/.local/var/spool/anacron"
+mkdir -p "${spooldir}"
 
-anacrontab=${XDG_CONFIG_HOME}/anacron/anacrontab
+anacrontab="${XDG_CONFIG_HOME}/anacron/anacrontab"
 
 [ ! -f "$anacrontab" ] && exit 0
 
-anacron -d -t $anacrontab -S $spooldir
+anacron -d -t "${anacrontab}" -S "${spooldir}"
